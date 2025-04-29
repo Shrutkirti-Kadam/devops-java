0327379ced1a8530f0fb152c16bc3336d6946214
Shrutkirti-Kadam <147423064+Shrutkirti-Kadam@users.noreply.github.com>
2025-04-29 23:34:48 +0530

    Create AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,9 @@
+// In main.java
+class AddTwoNumbers {  // Remove 'public' modifier
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
